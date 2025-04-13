afa0bc59595c9e5591325e17c900836203f0beb3
Manish Tirkey <asdmamanish@gmail.com>
2025-04-13 20:56:30 +0530

    updating main.js and snapshot.py

--- BackgroundProcess/snapShot.py
+++ BackgroundProcess/snapShot.py
@@ -9,6 +9,8 @@ if len(sys.argv) == 6:
     y2 = int(sys.argv[4])
     path = sys.argv[5]
 
+    print(x1, y1, x2, y2, path)
+
     screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
 
     screenshot.save(path)
